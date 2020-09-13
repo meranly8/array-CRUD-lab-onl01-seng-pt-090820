7c71f09f1797878429f82288baa1c6b68c0f7944
Michelle Ranly <meranly8@gmail.com>
2020-09-13 00:53:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Glacier", "Yellowstone", "Isle Royale", "Rocky Mountain"]
+  parks = ["Glacier", "Yellowstone", "Isle Royale", "Rocky Mountain"]
 end
 
 def add_element_to_end_of_array(array, element)
